aeb53fffec91bf7c724f3b8ef5d6eb48bdab9b88
hd2yao <1054290355@qq.com>
2025-09-17 17:16:55 +0800

    fix(user/model): 更正 model 中 created_time 的默认值为 CURRENT_TIMESTAMP

--- user/model/user.sql
+++ user/model/user.sql
@@ -8,7 +8,7 @@ CREATE TABLE `user` (
     `mail` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户邮箱',
     `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '手机号',
     `secret` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用来个人敏感信息处理',
-    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `mail_idx` (`mail`),
@@ -25,7 +25,7 @@ CREATE TABLE `user_address` (
     `city` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '市',
     `region` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '区',
     `detail_address` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '详细地址',
-    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`),
     KEY `uid` (`user_id`) USING BTREE
